967b8f441410b21d27adada85efb11a9270752b9
flp <2165649307@qq.com>
2025-09-18 17:37:33 +0800

    代码评审

---
+++ group-buy-market-app/src/test/java/cn/bugstack/test/TestReview.java
@@ -0,0 +1,11 @@
+package cn.bugstack.test;
+
+public class TestReview {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        if(1>2){
+            System.out.println("1>2");
+        }
+    }
+}
